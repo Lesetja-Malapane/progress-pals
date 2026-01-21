60f9cc0decaa83307669986f43fd44d02f70333c
Lesetja <kmalapane155@gmail.com>
2026-01-21 18:25:22 +0200

    feat: add devtools options, update HomePage with DotNavigationBar, and remove CustomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:progress_pals/app_router.dart';
-import 'package:rive/rive.dart';
-// import 'package:progress_pals/firebase_options.dart';
+import 'package:progress_pals/presentation/viewmodels/home_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await RiveNative.init();
-  
+
   runApp(const MyApp());
 }
 
@@ -18,13 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Progress Pals',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => HomeViewmodel())],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Progress Pals',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routerConfig: appRouter,
       ),
-      routerConfig: appRouter,
     );
   }
 }
--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,41 +1,68 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/web.dart';
-import 'package:progress_pals/core/theme/app_colors.dart';
-import 'package:progress_pals/presentation/pages/home/custom_navigation.dart';
 import 'package:progress_pals/presentation/viewmodels/home_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  static List<Widget> pages = [
-    Center(
-      child: FloatingActionButton(
-        onPressed: () => Logger().i('add a habit'),
-        tooltip: 'Add a habit',
-        child: Row(children: [Text('Add a Habit'), const Icon(Icons.add)]),
+  static final List<Widget> _pages = [
+    SafeArea(
+      child: Center(
+        child: FloatingActionButton(
+          onPressed: () => Logger().i('add a habit'),
+          tooltip: 'Add a habit',
+          child: const Icon(Icons.add),
+        ),
       ),
     ),
-    Center(child: Text("Manage Friends")),
-    Center(child: Text('Analytics')),
-    Center(child: Text('Profile')),
+    const Center(child: Text("Manage Friends")),
+    const Center(child: Text('Analytics')),
+    const Center(child: Text('Profile')),
   ];
+
+  final double _size = 32;
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => HomeViewmodel(),
-      child: Consumer<HomeViewmodel>(
-        builder: (context, homeViewModel, child) {
-          return Scaffold(
-            body: IndexedStack(
-              index: homeViewModel.selectedIndex,
-              children: pages,
-            ),
-
-            bottomNavigationBar: CustomNavigationBar(),
-          );
-        },
+    final homeViewModel = Provider.of<HomeViewmodel>(context);
+    return Scaffold(
+      body: _pages[homeViewModel.selectedIndex],
+      extendBody: true,
+      bottomNavigationBar: DotNavigationBar(
+        itemPadding: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+          top: 16,
+          bottom: 16,
+        ),
+        margin: const EdgeInsets.all(0),
+        marginR: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.05,
+          right: MediaQuery.of(context).size.width * 0.05,
+        ),
+        paddingR: const EdgeInsets.all(0),
+        currentIndex: homeViewModel.selectedIndex,
+        onTap: homeViewModel.setIndex,
+        items: [
+          DotNavigationBarItem(
+            icon: Icon(Icons.home),
+            selectedColor: Colors.purple,
+          ),
+          DotNavigationBarItem(
+            icon: Icon(Icons.people),
+            selectedColor: Colors.pink,
+          ),
+          DotNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            selectedColor: Colors.orange,
+          ),
+          DotNavigationBarItem(
+            icon: Icon(Icons.person),
+            selectedColor: Colors.teal,
+          ),
+        ],
       ),
     );
   }
